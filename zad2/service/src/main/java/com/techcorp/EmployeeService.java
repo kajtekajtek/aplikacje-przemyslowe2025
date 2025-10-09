e76e315062d3ac3c263521f27ac5f8ab5587f63d
kajtek <kajtek0123@gmail.com>
2025-10-09 12:48:07 +0200

    feat: add zad2 blueprint to zad2

---
+++ zad2/service/src/main/java/com/techcorp/EmployeeService.java
@@ -0,0 +1,69 @@
+package com.techcorp;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class EmployeeService 
+{
+    private List<Employee> employees;
+
+    public EmployeeService() { this.employees = new ArrayList<>(); }
+    
+    public void addEmployee(Employee employee) {
+        boolean emailExists = this.employees.stream()
+            .anyMatch(e -> e.getEmailAddress()
+                            .equalsIgnoreCase(employee.getEmailAddress())
+            );
+
+        if (emailExists) {
+            throw new IllegalArgumentException("Employee with email " + employee.getEmailAddress() + " already exists.");
+        }
+
+        this.employees.add(employee);
+    }
+    public void removeEmployee(Employee employee) { this.employees.remove(employee); }
+
+    public List<Employee> getEmployees() { return this.employees; }
+    public List<Employee> getEmployeesByCompanyName(String companyName) {
+        return this.employees.stream()
+            .filter(e -> e.getCompanyName().equalsIgnoreCase(companyName))
+            .collect(Collectors.toList());
+    }
+    public Map<Role, List<Employee>> getEmployeesByRole() {
+        return this.employees.stream()
+            .collect(Collectors.groupingBy(Employee::getRole));
+    }
+    public List<Employee> getEmployeesAlphabetically() {
+        return this.employees.stream()
+            .sorted(Comparator.comparing(Employee::getSurname))
+            .collect(Collectors.toList());
+    }
+    
+    public Map<Role, Long> getEmployeeCountByRole() {
+        return this.employees.stream()
+            .collect(Collectors.groupingBy(
+                Employee::getRole, Collectors.counting()
+            ));
+    }
+
+    public Optional<Employee> getEmployeeWithHighestSalary() {
+        return this.employees.stream()
+            .max(Comparator.comparing(Employee::getSalary));
+    }
+
+    public Double getAverageSalary() {
+        return this.employees.stream()
+            .mapToDouble(Employee::getSalary)
+            .average()
+            .orElse(0.0);
+    }
+    
+    public void printEmployees() {
+        this.employees.forEach(System.out::println);
+    }
+
+}
