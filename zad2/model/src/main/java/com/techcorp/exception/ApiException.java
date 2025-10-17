1bd9c420c0d65849ef47c0f9156d3b4a4111f784
kajtek <kajtek0123@gmail.com>
2025-10-17 08:09:39 +0200

    delete zad2

--- zad2/model/src/main/java/com/techcorp/exception/ApiException.java
+++
@@ -1,5 +0,0 @@
-package com.techcorp.exception;
-
-public class ApiException extends Exception {
-    public ApiException(String message) { super(message); }
-}
--- zad2/scripts/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -jar app/target/app-1.0-SNAPSHOT.jar
