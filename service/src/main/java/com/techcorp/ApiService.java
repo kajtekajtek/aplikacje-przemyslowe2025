b2e38434a3e104a8f0d4b5ff1adacc2f21d82ace
kajtek <kajtek0123@gmail.com>
2025-10-24 19:33:12 +0200

    use http client & gson beans in ApiService

--- service/src/main/java/com/techcorp/ApiService.java
+++ service/src/main/java/com/techcorp/ApiService.java
@@ -1,9 +1,10 @@
 package com.techcorp;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
 
 import com.techcorp.exception.ApiException;
 
@@ -15,21 +16,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 public class ApiService {
-    private HttpClient client;
+    private final HttpClient client;
+    private final Gson gson;
 
     @Value("${app.api.url}")
     private String apiUrl;
 
-    public ApiService(HttpClient client) {
+    public ApiService(HttpClient client, Gson gson) {
         this.client = client;
+        this.gson = gson;
     }
 
-    public List<Employee> fetchEmployeesFromApi() throws ApiException {
+    public List<Employee> fetchEmployeesFromApi(String apiUrl) throws ApiException {
         List<Employee> employees = new ArrayList<>();
 
         try {
@@ -39,8 +42,7 @@ public class ApiService {
                 throw new ApiException("HTTP error: " + response.statusCode());
             }
 
-            JsonArray jsonArray = JsonParser.parseString(response.body())
-                                            .getAsJsonArray();
+            JsonArray jsonArray = gson.fromJson(response.body(), JsonArray.class);
 
             for (JsonElement element : jsonArray) {
                 JsonObject jsonObject = element.getAsJsonObject();
@@ -50,9 +52,9 @@ public class ApiService {
                 employees.add(employee);
             }
 
-        } catch (com.google.gson.JsonSyntaxException e) {
+        } catch (JsonSyntaxException e) {
             throw new ApiException("Failed to parse JSON response: " + e.getMessage());
-        } catch (java.io.IOException e) {
+        } catch (IOException e) {
             throw new ApiException("Network error: " + e.getMessage());
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
