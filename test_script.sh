6e38a23592408659dfa90f5d294429949963732b
kajtek <kajtek0123@gmail.com>
2025-11-06 12:53:18 +0100

    improve test_script.sh

--- test_script.sh
+++ test_script.sh
@@ -1,34 +1,48 @@
 #!/bin/bash
 
-# GET wszystkich
+echo -e "\nGET wszystkich"
+echo -e "curl http://localhost:8080/api/employees\n"
+
 curl http://localhost:8080/api/employees -w "\n"
 
-# POST nowy pracownik
+echo -e "\nPOST nowy pracownik"
+echo -e "curl -X POST http://localhost:8080/api/employees\n"
+
 curl -X POST http://localhost:8080/api/employees \
   -H "Content-Type: application/json" \
-  -d '{"firstName":"Jan","lastName":"Kowalski","emailAddress":"jan@example.com","companyName":"TechCorp","role":"ENGINEER","salary":8000,"status":"ACTIVE"}' \
+  -d '{"firstName":"Grzegorz","lastName":"Kamieniec","emailAddress":"grzegorz@example.com","companyName":"TechCorp","role":"ENGINEER","salary":8000,"status":"ACTIVE"}' \
   -w "\n"
 
-# GET statystyki
+echo -e "\nGET statystyki"
+echo -e "curl http://localhost:8080/api/statistics/company/TechCorp\n"
+
 curl http://localhost:8080/api/statistics/company/TechCorp \
   -w "\n"
 
-# PUT aktualizacja
-curl -X PUT http://localhost:8080/api/employees/jan@example.com \
+echo -e "\nPUT aktualizacja"
+echo -e "curl -X PUT http://localhost:8080/api/employees/grzegorz@example.com\n"
+
+curl -X PUT http://localhost:8080/api/employees/grzegorz@example.com \
   -H "Content-Type: application/json" \
-  -d '{"firstName":"Jan","lastName":"Kowalski","emailAddress":"jan@example.com","companyName":"TechCorp","role":"MANAGER","salary":12000,"status":"ACTIVE"}' \
+  -d '{"firstName":"Grzegorz","lastName":"Kamieniec","emailAddress":"grzegorz@example.com","companyName":"TechCorp","role":"MANAGER","salary":12000,"status":"ACTIVE"}' \
   -w "\n"
 
-# PATCH zmiana statusu
-curl -X PATCH http://localhost:8080/api/employees/jan@example.com/status \
+echo -e "\nPATCH zmiana statusu"
+echo -e "curl -X PATCH http://localhost:8080/api/employees/grzegorz@example.com/status\n"
+
+curl -X PATCH http://localhost:8080/api/employees/grzegorz@example.com/status \
   -H "Content-Type: application/json" \
   -d '{"status":"ON_LEAVE"}' \
   -w "\n"
 
-# DELETE
-curl -X DELETE http://localhost:8080/api/employees/jan@example.com \
+echo -e "\nDELETE"
+echo -e "curl -X DELETE http://localhost:8080/api/employees/grzegorz@example.com\n"
+
+curl -X DELETE http://localhost:8080/api/employees/grzegorz@example.com \
   -w "\n"
 
-# GET statystyki
+echo -e "\nGET statystyki"
+echo -e "curl http://localhost:8080/api/statistics/company/TechCorp\n"
+
 curl http://localhost:8080/api/statistics/company/TechCorp \
   -w "\n"
