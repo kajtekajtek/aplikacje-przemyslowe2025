420dc1d0b7ccb50cd224d887e0577299dd0bdc1d
kajtek <kajtek0123@gmail.com>
2025-11-08 16:55:43 +0100

    add file handling exceptions

---
+++ model/src/main/java/com/techcorp/exception/FileNotFoundException.java
@@ -0,0 +1,7 @@
+package com.techcorp.exception;
+
+public class FileNotFoundException extends RuntimeException {
+    public FileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ model/src/main/java/com/techcorp/exception/FileStorageException.java
@@ -0,0 +1,7 @@
+package com.techcorp.exception;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
